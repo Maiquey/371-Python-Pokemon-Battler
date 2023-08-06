bba25c419a3c4ada14a6e103898cc191226a787a
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2023-08-06 12:30:13 -0700

    replace name

--- models/pokemon.py
+++ models/pokemon.py
@@ -1,11 +1,12 @@
 class Pokemon:
-    def __init__(self, name, ability, number, boosted_number):
+    def __init__(self, name, ability, number, boosted_number, boosted_name):
         self.hp = 100
         self.current_hp = 100
         self.name = name
         self.ability = ability
         self.number = number
         self.boosted_number = boosted_number
+        self.boosted_name = boosted_name
 
     def get_attacked(self, damage):
         self.current_hp -= damage
